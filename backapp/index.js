217d77c40ca36dc5f26588b311b70d201572ec30
Pryanshu <pryanshug88@gmail.com>
2024-09-18 20:10:08 +0530

    Updated DB

--- backapp/index.js
+++ backapp/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8000;
-const cors = require('cors');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoute");
+const queryRoute = require("./routes/queryRoute");
+const adRoute = require("./routes/adRoute");
 
-const mongoose = require('mongoose');
-const userRouter=require('./routes/userRoute');
-const queryRoute = require('./routes/queryRoute');
-const adRoute = require('./routes/adRoute')
+dotenv.config();
+const PORT = process.env.PORT || 8000;
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/karam')
-.then(()=>console.log("Connection Done 👍"))
-.catch((err)=>console.log("Error Found 😒",err));
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connection Done 👍"))
+  .catch((err) => console.log("Error Found 😒", err));
 
 //middleware
 app.use(express.json());
@@ -22,4 +22,4 @@ app.use(adRoute);
 app.use(queryRoute);
 
 app.use(userRouter);
-app.listen(port, () =>console.log(`Server Started on ${port}`));
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
